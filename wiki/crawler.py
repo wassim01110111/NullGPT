a8beba5dfeabc0b147fb28bb6b2de6ad309c8a3a
wassim01110111 <layachi.wassim.333@gmail.com>
2026-01-04 14:20:20 +0100

    update

---
+++ wiki/crawler.py
@@ -0,0 +1,5 @@
+end_lotm = 1394
+end_sidestories = 1445
+end_coi = end_sidestories + 1179
+weChat = "https://lordofthemysteries.fandom.com/wiki/Category:Archived"
+lotm_portal = "https://lordofthemysteries.fandom.com/wiki/Portal:Lord_of_Mysteries"
